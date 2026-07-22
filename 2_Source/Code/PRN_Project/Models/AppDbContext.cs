5e1c8f2e90079f0c77f07796ba1f3a5654f9e2ab
toanVd112 <toanvdhe187389@fpt.edu.vn>
2026-07-22 11:37:06 +0700

    feat(user): UC6 Quản lý Tài khoản (User Management)

--- 2_Source/Code/PRN_Project/Models/AppDbContext.cs
+++ 2_Source/Code/PRN_Project/Models/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
@@ -44,8 +44,13 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<VwPendingIncident> VwPendingIncidents { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=PRN_Project;User Id=sa;Password=123456;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=PRN_Project;User Id=sa;Password=123456;TrustServerCertificate=True;");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ 2_Source/Code/PRN_Project/Models/UserViewModels.cs
@@ -0,0 +1,50 @@
+using PRN_Project.Models;
+using System.Collections.Generic;
+
+namespace PRN_Project.Models
+{
+    public class UserListViewModel
+    {
+        public IEnumerable<User> Users { get; set; } = new List<User>();
+        public string? SearchTerm { get; set; }
+        public string? RoleFilter { get; set; }
+        public string? StatusFilter { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+
+    public class CreateUserViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng nhập Mã người dùng")]
+        [System.ComponentModel.DataAnnotations.StringLength(8, ErrorMessage = "Mã người dùng không vượt quá 8 ký tự")]
+        public string UserCode { get; set; } = null!;
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng nhập Họ và Tên")]
+        [System.ComponentModel.DataAnnotations.StringLength(150, ErrorMessage = "Họ và Tên không vượt quá 150 ký tự")]
+        public string FullName { get; set; } = null!;
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng nhập Email")]
+        [System.ComponentModel.DataAnnotations.EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; } = null!;
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng chọn Vai trò")]
+        public string Role { get; set; } = null!;
+    }
+
+    public class EditUserViewModel
+    {
+        public int UserId { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng nhập Mã người dùng")]
+        [System.ComponentModel.DataAnnotations.StringLength(8, ErrorMessage = "Mã người dùng không vượt quá 8 ký tự")]
+        public string UserCode { get; set; } = null!;
+
+        public string? FullName { get; set; } // Chỉ để hiển thị, không post lại
+
+        public string Email { get; set; } = null!;
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng chọn Vai trò")]
+        public string Role { get; set; } = null!;
+    }
+}
